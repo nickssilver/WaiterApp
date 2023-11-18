1d9543158174b3ed65a64399f423f1928bc17fe3
nickssilver <silvernicks9@gmail.com>
2023-11-18 11:32:40 +0300

    setups

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.waiterapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.waiterapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
