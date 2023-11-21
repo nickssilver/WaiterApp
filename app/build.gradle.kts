7fc74ee5573076edaeda2a0bd0e91479392dea4e
nickssilver <silvernicks9@gmail.com>
2023-11-21 18:20:18 +0300

    Login items Alignment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
     }
 
     buildTypes {
@@ -32,6 +33,7 @@ android {
 }
 
 dependencies {
+    implementation ("com.google.android.material:material:1.4.0")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
@@ -39,4 +41,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
